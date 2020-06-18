e9b3a81956715f618af4df2d568842df37ba83ff
couscousdude <youwenw@gmail.com>
2020-06-18 10:04:54 -0700

    Updates

---
+++ precache-manifest.b0078d46a5296bfd3a9c6e80429231db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091597defede26ce60a3715b2ffc7c30",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "f49f4c2509996a494f5e",
+    "url": "/react-app-test/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6a951cd5d837b3430780",
+    "url": "/react-app-test/static/js/2.b34b39b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-app-test/static/js/2.b34b39b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49f4c2509996a494f5e",
+    "url": "/react-app-test/static/js/main.4c979073.chunk.js"
+  },
+  {
+    "revision": "ffe3d1e794f72826295f",
+    "url": "/react-app-test/static/js/runtime-main.cc45c0c8.js"
+  }
+]);
